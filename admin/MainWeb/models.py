61f8828bac9df93335607b94ed029dca331f3f2b
Kobzar <ownpis030@gmail.com>
2024-10-17 07:32:16 +0300

    Добавлены модели в класс models

--- admin/MainWeb/models.py
+++ admin/MainWeb/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Seller(models.Model):
+    Prise = models.IntegerField(name='Цена')
+    Tovar = models.CharField(max_length=100,name= 'Товар')
+    Type = models.CharField(max_length=100,name= 'Тип')
+    model = models.CharField(max_length=100,name= 'Модель')
+    RAM = models.CharField(max_length=100,name= 'Оперативная память')
+    SSDMemory = models.CharField(max_length=100,name= 'Память')
+    DataRelise = models.DateField()
+    SizePhone = models.CharField(max_length=100,name= 'Разрешение телефона')
+    keyProduct = models.IntegerField()
+    garant = models.CharField(max_length=100,name='Гарантия')
+    image = models.ImageField(name='Фото', upload_to="./MainWeb/static/img")
+    
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
+    def __str__(self):
+        return self.Tovar
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
+class Acess(models.Model):
+    
+    Tovar = models.CharField(max_length=100,name='Аксесуар')
+    Prise = models.IntegerField(name='Цена')
+
+    class Meta:
+        verbose_name = "Аксесуар"
+        verbose_name_plural = "Аксесуары"
+
+    def __str__(self):
+        return self.Tovar
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
+class UserAnonim(User):
+    pass
+    
